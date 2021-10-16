e9f16b219a391547ec94a85e6f91bbd46c72ba0f
Jason Li <jasonli1337@gmail.com>
2021-10-16 15:38:31 -0400

    Chapter 8 except materials

--- Raytracing/Raytracing/vec3.h
+++ Raytracing/Raytracing/vec3.h
@@ -47,6 +47,21 @@ public:
 		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
 	}
 
+	inline static vec3 random() {
+		return vec3(random_double(), random_double(), random_double());
+	}
+
+	inline static vec3 random(double min, double max) {
+		return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+	}
+
+	bool near_zero() const {
+		//return true if the vector is close to zero in all dimensions.
+		const auto s = 1e-8;
+		// fabs() or floating point absolute value
+		return (fabs(e[0] < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s));
+	}
+
 	public:
 		double e[3];
 };
@@ -101,4 +116,28 @@ inline vec3 unit_vector(vec3 v) {
 }
 
 
+vec3 random_in_unit_sphere() {
+	while (true) {
+		auto p = vec3::random(-1, 1);
+		if (p.length_squared() >= 1) continue;
+		return p;
+	}
+}
+
+vec3 random_unit_vector() {
+	return unit_vector(random_in_unit_sphere());
+}
+
+vec3 random_in_hemisphere(const vec3& normal) {
+	vec3 in_unit_sphere = random_in_unit_sphere();
+	if (dot(in_unit_sphere, normal) > 0.0) //in the same hemisphere as the normal
+		return in_unit_sphere;
+	else
+		return -in_unit_sphere;
+}
+
+//there's a great diagram of this in the book
+vec3 reflect(const vec3& v, const vec3& n) {
+	return v - 2 * dot(v, n) * n;
+}
 #endif
