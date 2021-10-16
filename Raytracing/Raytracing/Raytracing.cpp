9af51d2cee06ef05a7b071e7e178daa87c1e7e20
Jason Li <jasonli1337@gmail.com>
2021-10-16 03:09:39 -0400

    Complete Chapter 6
    Added world

--- Raytracing/Raytracing/Raytracing.cpp
+++ Raytracing/Raytracing/Raytracing.cpp
@@ -1,41 +1,21 @@
 // Raytracing.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+
+#include "rtweekend.h"
+
 #include "color.h"
-#include "vec3.h"
-#include "ray.h"
+#include "hittable_list.h"
+#include "sphere.h"
 #include <iostream>
 
-//sphere hits
-//solved by finding the roots of a quadratic equation
-//one root for glancing, two roots for going through, imaginary roots for no intersection
-//the equation to find the ray sphere intersection is (P-C) * (P-C) = r^2
-//expanded into the (P(t) - C) * (P(t) - C) = r^2
-//and then further because P(t) = A + t*b; where A is origin and b is direction
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    auto a = r.direction().length_squared(); //equal to dot(r.direction(),r.direction());
-    auto half_b = dot(oc, r.direction()); // equal to b = 2.0 * dot(oc, r.direction());
-    auto c = oc.length_squared() - radius * radius; // dot(oc, oc) - radius * radius;
-    auto discriminant = half_b * half_b - a * c;//  b* b - 4 * a * c;
-    if (discriminant < 0) {
-        return -1.0;
-    }
-    else {
-        return(-half_b - sqrt(discriminant)) / (a);
-    }
-}
-
-
 //create a ray color function that evaluates a background
-color ray_color(const ray& r) {
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));//compute normal direction toward center of sphere
-        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
     }
-
     vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5 * (unit_direction.y() + 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
@@ -43,8 +23,14 @@ int main()
 {
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 1600;
+    const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // World
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
 
     // Camera
 
@@ -70,7 +56,7 @@ int main()
             auto v = double(j) / (image_height - 1);
             //The ray scans from bottom left and gradually evaluates upwards.
             ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r,world);
             write_color(std::cout, pixel_color);
         }
     }
